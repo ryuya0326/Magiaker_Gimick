ec84e44617d96fbc3630793ffe96db784efc6230
佐藤竜也 <satouryuya0326@gmail.com>
2018-02-09 17:40:06 +0900

    自分が作業したスクリプト

---
+++ Gimick/G_M_House2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G_M_House2 : Gimick {
+    [SerializeField]
+    List<GameObject> WallList = new List<GameObject>();
+
+    [SerializeField]
+    List<GameObject> EnemyList = new List<GameObject>();
+	
+	// Update is called once per frame
+	void Update () {
+        int x = EnemyList.Count - 1;
+
+        //常にモンスターがリストからいなくなったらリストを削除する
+        for (int i = 0; i <= x; i++)
+        {
+            if (EnemyList[i] == null)
+            {
+                EnemyList.Remove(EnemyList[i]);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //プレイヤーがモンスターハウス内に入った時
+        if (other.gameObject.tag == Tags.Player)
+        {
+            //壁を作る
+            for (int i = 0; i < WallList.Count; i++)
+            {
+                WallList[i].SetActive(true);
+            }
+            //enemyが出現
+            for (int i = 0; i < EnemyList.Count; i++)
+            {
+                EnemyList[i].SetActive(true);
+            }
+        }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        //Enemyがいなくなったとき
+        //壁がなくなる
+        if (EnemyList.Count == 0)
+        {
+            if (other.gameObject.tag == Tags.Player)
+            {
+                for (int i = 0; i < WallList.Count; i++)
+                {
+                    WallList[i].SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Gimick/G_Switch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G_Switch : Gimick {
+
+    float HP = 1.0f;
+
+    [SerializeField]
+    List<GameObject> WallList = new List<GameObject>();
+
+    AttackArea AttackMagic;
+
+    private GameObject _child;
+    private void Start()
+    {
+        int count = 0;
+        foreach (Transform child in transform)
+        {
+            GameObject Childwall = GameObject.Find(child.name);
+            WallList.Add(Childwall);
+            count++;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //AttackAreaのついている攻撃が来たら
+        if (other.gameObject.GetComponent<AttackArea>())
+        {
+            AttackMagic = other.gameObject.GetComponent<AttackArea>();
+            //プレイヤーの攻撃でしか壊せない
+            if (AttackMagic.aligment == aligment.player)
+            {
+                HP -= AttackMagic.Damage;
+                if (HP <= 0)
+                {
+                    //HPを減らす攻撃であれば壊れる
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
